19ae6bfef13cf7d4599ac4f78f3b8603d1edd05c
韓 勇治 <kan-y@mc24606-bp20-2.gndomain.gnavi.co.jp>
2020-10-12 10:42:04 +0900

    mod manage.py

---
+++ manage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+import sys
+import unittest
+sys.path.append(os.getcwd())
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
